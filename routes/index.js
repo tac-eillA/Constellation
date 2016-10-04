16ea0c6a489580636b234fe006e94f8061973986
tsnodgrass96 <trevorsemail96@gmail.com>
2016-10-04 12:45:43 -0500

    added basic login and landing page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Constellation' });
 });
 
+router.post('/', function (req, res) {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    res.send('Post page');
+});
+
 module.exports = router;
