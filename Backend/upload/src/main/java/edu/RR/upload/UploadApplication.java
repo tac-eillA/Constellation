cd664e1ee0673d02b17dd70871ea0d51e7c4207e
jgibbygibs <jgibson37@gatech.edu>
2016-10-07 20:09:42 -0400

    removed unused file

--- Backend/upload/src/main/java/edu/RR/upload/UploadApplication.java
+++ Backend/upload/src/main/java/edu/RR/upload/UploadApplication.java
@@ -13,7 +13,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @CrossOrigin
-//@Import(SecurityConfiguration.class)
 public class UploadApplication  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(UploadApplication.class);
