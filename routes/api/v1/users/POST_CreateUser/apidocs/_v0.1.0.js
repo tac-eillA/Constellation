668093a8b7e8a404f255a3d2525d6faff14776aa
th3n3wguy <tarleton.shane@gmail.com>
2016-10-11 23:47:26 -0500

    Added the initial API documentation for the v1 POST_CreateUser (POST /api/v1/users) route. Will update later with more details.

---
+++ routes/api/v1/users/POST_CreateUser/apidocs/_v0.1.0.js
@@ -0,0 +1,10 @@
+/**
+ * @api {POST} /api/v1/users Create User
+ * @apiVersion 0.1.0
+ * @apiName CreateUser
+ * @apiGroup User
+ *
+ * @apiParam {String{8..255}} username The username for the user's profile.
+ * @apiParam {String{3..255}} emailAddress The email address for the user's profile.
+ * @apiParam {String{8..255}} password The password for the user's profile.
+ */
