63a751071e792f6bf931e3b64bd8498b983b1f8c
th3n3wguy <tarleton.shane@gmail.com>
2016-10-24 14:13:15 -0500

    Added the length to the string for the password.

--- models/identity/User.js
+++ models/identity/User.js
@@ -42,7 +42,7 @@ module.exports = function(sequelize, DataTypes) {
                 }
             },
             password: {
-                type: DataTypes.STRING,
+                type: DataTypes.STRING(60),
                 field: 'password',
                 allowNull: false,
                 validate: {
